236b5806ce812c9d34af8e7790e5cb53b3d7ac7d
Diem Tran <emma.trandiem@gmail.com>
2026-04-26 02:34:33 +0300

    env.vault added

--- helpers/db.js
+++ helpers/db.js
@@ -18,8 +18,9 @@ const openDb = () => {
         user: process.env.DB_USER,
         host: process.env.DB_HOST,
         database: process.env.DB_NAME,
-        password: String(process.env.DB_PASSWORD),
+        password: process.env.DB_PASSWORD,
         port: process.env.DB_PORT,
+        ssl: process.env.SSL
     })
     return pool
 }
